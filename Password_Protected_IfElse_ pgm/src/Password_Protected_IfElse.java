4828ced540ea7a8379627566ecbcb02b290033c5
Tanveer Dar <tanveerdar163@gmail.com>
2023-05-08 19:21:44 +0530

    Update Password_Protected_IfElse.java

--- Password_Protected_IfElse_ pgm/src/Password_Protected_IfElse.java
+++ Password_Protected_IfElse_ pgm/src/Password_Protected_IfElse.java
@@ -13,12 +13,14 @@ public class Password_Protected_IfElse {
             System.out.println("Enter your marks");
             int marks = sc.nextInt();
 
-            if(marks>60){
+            if(marks<=100 && marks>=60){
                 System.out.println("Grade A");
-            }else if(marks>30){
+            }else if(marks<60 && marks>=30){
                 System.out.println("Grade B");
-            }else{
+            }else if(marks<30 && marks>=0){
                 System.out.println("Fail");
+            }else{
+                System.out.println("invalid input");
             }
         }else{
             System.out.println("Access denied");
