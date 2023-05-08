feeb0eaba46248b76b99da789ef557f93f01268f
Tanveer Dar <tanveerdar163@gmail.com>
2023-05-08 19:10:57 +0530

    Add files via upload

---
+++ Password_Protected_IfElse_ pgm/src/Password_Protected_IfElse.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Password_Protected_IfElse {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String secretPassword="Pass1234";
+        System.out.println("Enter your password");
+        String inPassword =sc.next();
+
+        if(inPassword.equals(secretPassword)){
+            System.out.println("access granted");
+
+            System.out.println("Enter your marks");
+            int marks = sc.nextInt();
+
+            if(marks>60){
+                System.out.println("Grade A");
+            }else if(marks>30){
+                System.out.println("Grade B");
+            }else{
+                System.out.println("Fail");
+            }
+        }else{
+            System.out.println("Access denied");
+        }
+    }
+}
